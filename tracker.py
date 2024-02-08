37fce2e331c4e8a501297c46d2728f3217064a38
SantiagoGiraldoM17 <santiago.giraldom@upb.edu.co>
2024-02-07 20:16:03 -0500

    Added comments on important parts of the code

--- tracker.py
+++ tracker.py
@@ -1,12 +1,11 @@
 import numpy as np 
 import cv2
 import matplotlib.pyplot as plt
-import IPython.display as ipd
 
 class ScaleSetter:
     def __init__(self, image):
         self.ref_points = []
-        self.image = image.copy()  # Make a copy of the image to draw on
+        self.image = image.copy() 
         self.scale_cm_per_pixel = None
 
     def click_and_mark(self, event, x, y, flags, param):
@@ -22,11 +21,11 @@ class ScaleSetter:
 
     def set_scale(self):
         """Invoke this method to set the scale by manually marking two points on the image."""
-        cv2.namedWindow("Image")
-        cv2.setMouseCallback("Image", self.click_and_mark)
+        cv2.namedWindow("Scale")
+        cv2.setMouseCallback("Scale", self.click_and_mark)
 
         while True:
-            cv2.imshow("Image", self.image)
+            cv2.imshow("Scale", self.image)
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q') or len(self.ref_points) == 2:  # Quit or two points have been marked
                 break
@@ -45,7 +44,6 @@ def drawBox(img, bbox):
 
 cap = cv2.VideoCapture("CaidaLibre.mp4")
 frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-fps = cap.get(cv2.CAP_PROP_FPS)
 
 tracker = cv2.legacy.TrackerMOSSE.create()
 ret, img = cap.read()
@@ -62,24 +60,26 @@ img_idx = 0
 
 list_y = []
 list_frames = []
+
 for frame in range(frames):
     ret, img = cap.read()
     if ret == False:
         break
+
     ret1, bbox = tracker.update(img)
-    if ret1:
-        if frame % 10 == 0:
-            drawBox(img, bbox)
-            axs[img_idx].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-            axs[img_idx].set_title(f'Frame: {frame}')
-            axs[img_idx].axis('off')
-            img_idx += 1
-            y = int(bbox[1])
-            list_y.append(y)
-            list_frames.append(frame)
+    if frame % 10 == 0:
+        drawBox(img, bbox)
+        axs[img_idx].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        axs[img_idx].set_title(f'Frame: {frame}')
+        axs[img_idx].axis('off')
+        img_idx += 1
+        y = int(bbox[1])
+        list_y.append(y)
+        list_frames.append(frame)
+
         
 
-times = np.array([list_frames[i] / 1000 for i in range(len(list_y))])
+times = np.array([list_frames[i] / 1000 for i in range(len(list_y))]) # Time in seconds
 distances_pixels = np.array([list_y[i] for i in range(len(list_y))])  # Distance in pixels
 distances_mm = ((distances_pixels - distances_pixels[0]) * scale_cm_per_pixel * 10)
 
@@ -87,24 +87,22 @@ time_differences = np.diff(times)
 distance_differences = np.diff(distances_mm)
 speeds_mm_per_s = distance_differences / time_differences
 
-distances_m = distances_mm / 1000
-speeds_m_per_s = speeds_mm_per_s / 1000
 
 # Plot Distance vs. Time
 plt.figure(figsize=(15, 5))
 plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
-plt.plot(times, distances_m, '-o')
+plt.plot(times[4:], distances_mm[4:], '-o')
 plt.title('Distance vs. Time')
 plt.xlabel('Time (seconds)')
-plt.ylabel('Distance (meters)')
+plt.ylabel('Distance (milimeters)')
 plt.grid(True)
 
 # Plot Speed vs. Time
 plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
-plt.plot(times[1:], speeds_m_per_s, '-o')  # Exclude the first time point as speeds array is one less
+plt.plot(times[5:], speeds_mm_per_s[4:], '-o')  # Exclude the first time point as speeds array is one less
 plt.title('Speed vs. Time')
 plt.xlabel('Time (seconds)')
-plt.ylabel('Speed (m/s)')
+plt.ylabel('Speed (mm/s)')
 plt.grid(True)
 
 plt.tight_layout()
